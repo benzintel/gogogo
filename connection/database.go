ac124b7e46765dcd7b20f04639484b6497375f6c
sarut yentakham <sarut.y@bitkub.com>
2018-05-31 00:06:13 +0700

    Update Connection MongoDB

--- connection/database.go
+++ connection/database.go
@@ -6,6 +6,7 @@ import (
 	"database/sql"
 	"github.com/joho/godotenv"
 	_ "github.com/go-sql-driver/mysql"
+	"gopkg.in/mgo.v2"
 )
 
 type ConfigDatabase struct {
@@ -14,20 +15,20 @@ type ConfigDatabase struct {
 	Password 	string
 	Host 		string
 	Port 		string
-	Name 		string
+	Name 		string // Database
+	Collection 	string // collection
 }
 
 func SetConfig(config []string) ConfigDatabase{
-	return ConfigDatabase{ Driver: config[0], Username: config[1], Password: config[2], Host: config[3], Port: config[4], Name: config[5]}
+	return ConfigDatabase{ Driver: config[0], Username: config[1], Password: config[2], Host: config[3], Port: config[4], Name: config[5], Collection: "" }
 }
 
 
 // https://github.com/go-sql-driver/mysql
-
 func GetConnectionMysql(dbConnection []string) (*sql.DB){
 
 	configDB := SetConfig(dbConnection)
-	fmt.Println(configDB.Driver, configDB.Username, configDB.Password, configDB.Host, configDB.Password, configDB.Name)
+	// fmt.Println(configDB.Driver, configDB.Username, configDB.Password, configDB.Host, configDB.Password, configDB.Name)
 
 	fmt.Println("Connection...")
 	db, err := sql.Open( configDB.Driver, configDB.Username + ":" + configDB.Password + "@tcp(" + configDB.Host + ":" + configDB.Port + ")/" + configDB.Name + "?timeout=5s" )
@@ -43,6 +44,26 @@ func GetConnectionMysql(dbConnection []string) (*sql.DB){
 
 	fmt.Println("Connection Success")
 	return db
+}
+
+func GetConnectionMongoDB(dbConnection []string) (*mgo.Session) {
+
+	configDB := SetConfig(dbConnection)
+
+	var url string
+	if len(configDB.Username) > 0 {
+		url = "mongodb://" + configDB.Username + ":" + configDB.Password + "@" + configDB.Host + ":" + configDB.Port
+	} else {
+		url = "mongodb://" + configDB.Host + ":" + configDB.Port
+	}
+
+	session, err := mgo.Dial(url)
+	if err != nil {
+		fmt.Println("Connection Mongo Fail 01")
+		return nil
+	}
+
+	return session	
 }
 
 func GetENV() {
